09e5a8d0f0bcb691a77de1574e743c33d85f37ad
Felipe Coury <felipe.coury@gmail.com>
2021-09-08 15:11:17 -0300

    feat: added script to backup zsh_history

---
+++ history-backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DATE=`date "+%Y%m%d"`
+#cp ~/.zsh_history ~/Dropbox/history/.zsh_history_$HOST_$DATE
+/usr/bin/zip /Users/fcoury/Dropbox/history/.zsh_history_$HOST_$DATE.zip /Users/fcoury/.zsh_history
